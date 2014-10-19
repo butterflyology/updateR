d984c8718686cb0bf38580fe68140a1b12296025
Chris Hamm <chamm@ku.edu>
2014-10-19 11:31:57 -0500

    Initial commit

---
+++ updateR.R
@@ -0,0 +1,15 @@
+#updateR, by CA Hamm (version 0.1) 19 October 2014
+
+#--run in the old version of R
+setwd("~/Directory/WhereYouWantIt")
+packages <- installed.packages()[,"Package"]
+save(packages, file="Rpackages")
+
+#--run in the new version
+setwd("~/Directory/WhereYouPutIt")
+load("Rpackages")
+for (p in setdiff(packages, installed.packages()[,"Package"]))
+install.packages(p)
+
+# To remove all packages except the base
+#remove.packages(packages)
