5f2969f0205e6ee8904bc435bd6a611ab574502b
Chris Hamm <butterflyology@users.noreply.github.com>
2017-06-08 13:35:13 -0500

    Updated formatting
    Updated code formatting to follow google style guide.

--- updateR.R
+++ updateR.R
@@ -2,13 +2,13 @@
 
 #--run in the old version of R
 setwd("~/Directory/WhereYouWantIt")
-packages <- installed.packages()[,"Package"]
-save(packages, file="Rpackages")
+packages <- installed.packages()[, "Package"]
+save(packages, file = "Rpackages")
 
 #--run in the new version
 setwd("~/Directory/WhereYouPutIt")
 load("Rpackages")
-for (p in setdiff(packages, installed.packages()[,"Package"]))
+for (p in setdiff(packages, installed.packages()[, "Package"]))
 install.packages(p)
 
 # To remove all packages except the base
